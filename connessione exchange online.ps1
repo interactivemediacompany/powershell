5b5f4114d360a249ab632761be5801be92c106e8
mmangiante <mmangiante@imnet.com>
2016-04-07 15:12:08 +0200

    Exchange Online
    Comandi per la connessione in PS ad Exchange e abilita/disabilità di
    proprietà riguardanti le cassette postali condivise

---
+++ connessione exchange online.ps1
@@ -0,0 +1,23 @@
+﻿# Eseguire questo comando (con privilegi da amministratore) solo la prima volta che si configura l'accesso in Powershell
+
+Set-ExecutionPolicy RemoteSigned
+
+# Primo passo, registrare le credenziali di accesso ad Exchange Online nella variabile (viene presentata una dialog in cui inserire le credenziali)
+
+$UserCredential = Get-Credential
+
+# Creare una nuova sessione
+
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+
+# Importare la sessione
+
+Import-PSSession $Session
+
+#Per fare un test che tutto sia andato a buon fine, dare un comando, per esempio 
+
+Get-Mailbox
+
+# Una volta finite le operazioni, dare il comando di disconnessione dalla sessione
+
+Remove-PSSession $Session
---
+++ sent mail folder activation-disactivation exchange online.ps1
@@ -0,0 +1,19 @@
+﻿# Per far si che le mail inviate da una cassetta postale condivisa siano salvate anche nella cartella "Posta Inviata" 
+# di quest'ultima bisogna dare i seguenti comandi Powershell
+
+# Per mail inviate come "Sent as.."
+
+set-mailbox <mailbox name> -MessageCopyForSentAsEnabled $True
+
+# Per mail inviate come "Sent On Behalf.."
+
+set-mailbox <mailbox name> -MessageCopyForSendOnBehalfEnabled $True
+
+# Per disabilitare queste proprietà
+
+set-mailbox <mailbox name> -MessageCopyForSentAsEnabled $False
+
+set-mailbox <mailbox name> -MessageCopyForSendOnBehalfEnabled $False
+
+# dove <mailbox name> è il nome della cassetta postale condivisa: può essere utilizzato anche l'alias ottenuto con 
+# il comando Get-Mailbox
