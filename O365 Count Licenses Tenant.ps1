bc09bf70943ed4b93cf00f1e7af27b3f6de874ed
mmangiante <mmangiante@imnet.com>
2016-04-18 12:05:01 +0200

    Added procedure to connect to Office 365 and count licenses

---
+++ O365 Count Licenses Tenant.ps1
@@ -0,0 +1,11 @@
+﻿# Importa il modulo di Azure Active Direcory
+Import-Module MSOnline
+
+# cmdlet per connettersi col servizio online
+Connect-MsolService
+
+# Salva nella variabile tutti gli SKU di cui la società è proprietaria
+$AccountSku = Get-MsolAccountSku
+
+# Numero di licenze disponibile al Tenant
+$AccountSku.Count
