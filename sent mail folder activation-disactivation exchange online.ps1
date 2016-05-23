d6c72577b94d262eb32e2f1769029f07ab90854c
mmangiante <mmangiante@imnet.com>
2016-05-23 17:41:08 +0200

    Added line for disabling autodiscover
    With these commands is it possible to disable autodiscover for a
    particulare shared mail related to a user

--- sent mail folder activation-disactivation exchange online.ps1
+++ sent mail folder activation-disactivation exchange online.ps1
@@ -17,3 +17,12 @@ set-mailbox <mailbox name> -MessageCopyForSendOnBehalfEnabled $False
 
 # dove <mailbox name> è il nome della cassetta postale condivisa: può essere utilizzato anche l'alias ottenuto con 
 # il comando Get-Mailbox
+
+# Per disabilitare l'autodiscover di una mailbox condivisa, in modo che non compaia nel client, dare questi 2 comandi
+# dove [shared mailbox] è l'indirizzo condiviso e [user mailbox] è l'indirizzo dell'utente su cui deve essere disabilitato
+# l'autodiscover per quella particolare mail condivisa
+
+Remove-MailboxPermission -Identity [shared mailbox] -User [user mailbox] -AccessRights FullAccess
+
+Add-MailboxPermission -Identity [shared mailbox] -User [user mailbox] -AccessRights FullAccess -AutoMapping:$false
+
